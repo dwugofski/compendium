9dee1477c07288d95fd4ba0a6d37d35967015e96
dwugofsk <david@wugofski.com>
2018-10-26 15:33:29 -0500

    Revisions and refinement of logging in and signing up

--- com/js/def.login.js
+++ com/js/def.login.js
@@ -5,15 +5,42 @@ Compendium.Login = {
 	screen: undefined,
 	navopt: undefined,
 
+	switch_to_signup : undefined,
+	login_submit : undefined,
+	login_form_username : undefined,
+	login_form_password : undefined,
+
+	switch_to_login : undefined,
+	signup_submit : undefined,
+
+	error_box : undefined,
+
 	init : function() {
 		this.login_form = $("#login_form");
 		this.signup_form = $("#signup_form");
 		this.screen = $("#login_screen");
 		this.navopt = $("#navopt_sign_in");
 
-		$("#login_sign_up").click({action: "signup"}, this.switch_login_screen.bind(this));
-		$("#signup_log_in").click({action: "login"}, this.switch_login_screen.bind(this));
-		$("#login_form_submit").click({action: "login"}, this.submit_login_form.bind(this));
+		this.switch_to_signup = $("#login_sign_up");
+		this.login_submit = $("#login_form_submit");
+		this.login_form_username = $("#login_form_username");
+		this.login_form_password = $("#login_form_password");
+
+		this.switch_to_login = $("#signup_log_in");
+		this.signup_submit = $("#signup_form_submit");
+
+		this.error_box = $("#login_error");
+
+		this.switch_to_signup.click({action: "signup"}, this.switch_login_screen.bind(this));
+		this.switch_to_login.click({action: "login"}, this.switch_login_screen.bind(this));
+
+		this.login_submit.click(this.submit_login_form.bind(this));
+
+		this.login_form_username.on("input", this.update_login_field.bind(this));
+		this.login_form_password.on("input", this.update_login_field.bind(this));
+
+		this.disable_login();
+		this.disable_signup();
 
 		this.navopt.click(this.display_login_screen.bind(this));
 		this.screen.click(function(event) {
@@ -21,6 +48,74 @@ Compendium.Login = {
 				this.hide_login_screen(event);
 			}
 		}.bind(this));
+	},
+
+	verify_email : function(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	},
+
+	verify_password : function(password) {
+		var re = /^^([a-zA-Z0-9\[\]<>()\\\/,.?;:'"{}|`~!@#$%^&*\-_=+\ ]{8,100})$/;
+		return re.test(String(password));
+	},
+
+	verify_username : function(username) {
+		var re = /([a-zA-Z])([\w]{2,24})$/;
+		return re.test(String(username));
+	},
+
+	show_error : function(str) {
+		if (this.error_box.css("display") == "none") this.error_box.slideDown();
+		this.error_box.html(str);
+	},
+
+	update_login_field : function(e) {
+		var okay = true;
+
+		console.log("Changed");
+
+		okay = okay && (this.login_form_username.val() && 0 !== this.login_form_username.val().length);
+		okay = okay && this.verify_username(this.login_form_username.val());
+		okay = okay && (this.login_form_password.val() && 0 !== this.login_form_password.val().length);
+		okay = okay && this.verify_password(this.login_form_password.val());
+		if (okay) {
+			console.log("password okay");
+		}
+
+		if (okay) {
+			this.enable_login();
+		} else {
+			this.disable_login();
+		}
+	},
+
+	disable_login : function() {
+		if (!this.login_submit.hasClass("disabled")) {
+			this.login_submit.addClass("disabled");
+		}
+		this.login_submit.disabled = true;
+	},
+
+	enable_login : function() {
+		if (this.login_submit.hasClass("disabled")) {
+			this.login_submit.removeClass("disabled");
+		}
+		this.login_submit.disabled = false;
+	},
+
+	disable_signup : function() {
+		if (!this.signup_submit.hasClass("disabled")) {
+			this.signup_submit.addClass("disabled");
+		}
+		this.signup_submit.disabled = true;
+	},
+
+	enable_signup : function() {
+		if (this.signup_submit.hasClass("disabled")) {
+			this.signup_submit.removeClass("disabled");
+		}
+		this.signup_submit.disabled = false;
 	},
 
 	display_login_screen : function(event) {
@@ -44,20 +139,21 @@ Compendium.Login = {
 		}
 	},
 
-	submit_login_form : function() {
-		console.log("Running");
-		var login_data = this.login_form.serializeArray().reduce(function(o, i){ 
-			o[i.name] = i.value;
-			return o;
-		}, {});
-		var php_data = {"userident": login_data.username, "password":login_data.password};
-		$.ajax({
-			url : "com/php/login_user.php",
-			type : "POST",
-			data : php_data,
-			success : this.handle_php_login.bind(this),
-			error : this.handle_php_login_error.bind(this)
-		});
+	submit_login_form : function(e) {
+		if (!this.login_submit.disabled) {
+			var login_data = this.login_form.serializeArray().reduce(function(o, i){ 
+				o[i.name] = i.value;
+				return o;
+			}, {});
+			var php_data = {"userident": login_data.username, "password":login_data.password};
+			$.ajax({
+				url : "com/php/login_user.php",
+				type : "POST",
+				data : php_data,
+				success : this.handle_php_login.bind(this),
+				error : this.handle_php_login_error.bind(this)
+			});
+		}
 	},
 
 	handle_php_login : function(data, status, jqxhr) {
---
+++ com/php/signup_user.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once(__DIR__."/session.php");
+include_once(__DIR__."/errors.php");
+include_once(__DIR__."/user.php");
+
+// TODO: Limit login attempts -- gate login with capcha after certain # attempts
+// Will need to create login table for login attempts from ip address
+// Probably will want three levels: immediate access, delayed access, capcha verification
+
+try {
+
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['pasword'];
+
+	if (!User::validate_username($username)) throw new CompendiumError("Username invalid", TRUE, ERRORS::USER_ERROR);
+	if (!User::validate_email($email)) throw new CompendiumError("Email invalid", TRUE, ERRORS::USER_ERROR);
+	if (User::check_user($username)) throw new CompendiumError("Username already taken", TRUE, ERRORS::USER_ERROR);
+	if (User::check_email($email)) throw new CompendiumError("Email already taken", TRUE, ERRORS::USER_ERROR);
+
+	$user = User::create_new_user($username, $password, $email);
+
+	$_SESSION['user'] = $user;
+	json_ret($user->data);
+}
+catch (CompendiumError $e) {
+	ERRORS::json_log($e);
+}
+
+
+
+?>
